55c36fb67777c26594c1816b170a595414dcdbee
Alexandr Izmaylov <zinoviy23@gmail.com>
2018-05-03 19:35:35 +0300

    negation button

--- WinFormCourseWork/PermutationCalculator.cs
+++ WinFormCourseWork/PermutationCalculator.cs
@@ -72,14 +72,55 @@ namespace WinFormCourseWork
             if (inputButton == null) 
                 return;
             inputButton.Click += InputButtonOnClick;
+
+            var deleteButton = _htmlView.Document?.GetElementById("delete_button");
+            if (deleteButton == null)
+                return;
+            deleteButton.Click += DeleteButtonOnClick;
+
+            var negationButton = _htmlView.Document?.GetElementById("negation_button");
+            if (negationButton == null)
+                return;
+            negationButton.Click += NegationButtonOnClick;
+        }
+
+        /// <summary>
+        /// Обработчик события нажатия на кнопку обратной подстановки. Отрисовывает обратную подстановку.
+        /// </summary>
+        /// <param name="sender">Кнопка</param>
+        /// <param name="e">Аргументы события</param>
+        private void NegationButtonOnClick(object sender, HtmlElementEventArgs e)
+        {
+            if (_showingPermutation == null)
+            {
+                MessageBox.Show(@"Введите подстановку!", @"Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            var negPerm = -_showingPermutation;
+            WriteNegationInformation(_showingPermutation, negPerm);
+            _showingPermutation = negPerm;
+            ShowCurrentPermutation();
+        }
+
+        /// <summary>
+        /// Сбрасывает всю информацию о подстановках
+        /// </summary>
+        /// <param name="sender">Кнопка</param>
+        /// <param name="e">Аргументы события</param>
+        private void DeleteButtonOnClick(object sender, HtmlElementEventArgs e)
+        {
+            _savedPermutation = null;
+            _showingPermutation = null;
+            ShowCurrentPermutation();
         }
 
         /// <summary>
         /// Обработчик события нажатия на кнопку ввести. Отображает диалог и отрисовывает введённую подстановку
         /// </summary>
-        /// <param name="sender">браузер</param>
+        /// <param name="sender">Кнопка</param>
         /// <param name="args">аргументы события</param>
-        private void InputButtonOnClick(object sender, EventArgs args)
+        private void InputButtonOnClick(object sender, HtmlElementEventArgs args)
         {
             var permutationInputDialog = new PermutationInput();
             permutationInputDialog.ShowDialog();
@@ -102,6 +143,27 @@ namespace WinFormCourseWork
             permutationElement.InnerHtml = _showingPermutation == null
                 ? ""
                 : PermutationVisualisation.ListOfTuplesToHtml(_showingPermutation.TupleList);
+        }
+
+        /// <summary>
+        /// Отрисовывает информацию о действии "Обратная подстановка".
+        /// </summary>
+        /// <param name="prev">подстановка</param>
+        /// <param name="newPerm">обратная к ней</param>
+        private void WriteNegationInformation([NotNull] Permutation prev, [NotNull] Permutation newPerm)
+        {
+            var hisotoryDiv = _htmlView.Document?.GetElementById("history");
+            var newDiv = _htmlView.Document?.CreateElement("div");
+            if (hisotoryDiv == null || newDiv == null)
+                return;
+
+            newDiv.InnerHtml =
+                "<table><tr><td>Обратная подстановка к</td>" +
+                $"<td>{PermutationVisualisation.ListOfTuplesToHtml(prev.TupleList)}</td>" +
+                "<td>это</td>" +
+                $"<td>{PermutationVisualisation.ListOfTuplesToHtml(newPerm.TupleList)}</td></tr></table>";
+
+            hisotoryDiv.AppendChild(newDiv);
         }
     }
 }
